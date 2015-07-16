5b31b3f90b5edf3262245db74beebbb5f5082be0
Adam Bray <adam.bray@yale.edu>
2015-07-16 10:08:11 -0400

    added in class solution (OOP)

--- atm.js
+++ atm.js
@@ -1,21 +1,60 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+var atm = {
+  accounts: {
+    checking: 0,
+    savings: 0
+  },
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+  initialize: function() {
+    $(".account").on("click", function(event) {
+      event.preventDefault(); // don't go to a new page
+
+      var actionType = $(event.target).val(); // Deposit or Withdrawl
+      var accountType = $(event.target).siblings("h2").eq(0).html() // Checking or Savings
+      var amount = parseInt($(event.target).siblings("[type=text]").val());
+
+      if(actionType === "Deposit") {
+        this.deposit(amount, accountType);
+      }else if(actionType === "Withdraw"){
+        this.withdraw(amount, accountType);
+      }
+    }.bind(this)); // this refers to `atm`, so we bind the callback to `atm`
+
+  },
+
+  deposit: function(amount, account) {
+    // use dynamic property to avoid "if statement"
+    account = account.toLowerCase(); // "Checking", "checking", "Savings", "savings"
+    this.accounts[account] += amount;
+
+    // more traditional if statement alternative
+    // if(account === "checking") {
+    //   this.accounts.checking += amount;
+    // } else if(account === "savings") {
+    //   this.accounts.savings += amount;
+    // }
+    this.updateDisplay();
+
+  },
+
+  withdraw: function(amount, account) {
+    account = account.toLowerCase(); // "Checking", "checking", "Savings", "savings"
+    this.accounts[account] -= amount;
+
+    // more traditional if statement alternative
+    // if(account === "checking") {
+    //   this.accounts.checking -= amount;
+    // } else if(account === "savings") {
+    //   this.accounts.savings -= amount;
+    // }
+
+    this.updateDisplay();
+  },
+
+  updateDisplay: function() {
+    $(".balance").eq(0).html("$" + this.accounts.checking);
+    $(".balance").eq(1).html("$" + this.accounts.savings);
+  }
 
 }
 
-function checkingWithdrawal(amount){
-
-}
-
-function savingsDeposit(amount){
-
-}
-
-function savingsWithdrawal(amount){
-
-}
-
-// an eventListerner for each button, each one a "click"
+atm.initialize();
