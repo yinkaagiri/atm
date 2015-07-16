85913ef9ffc30e9735f3b52e834b9a4514d208d7
Jesse Shawl <jesse@jshawl.com>
2015-07-16 09:53:48 -0400

    add in class solution

--- atm.js
+++ atm.js
@@ -1,21 +1,29 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
+var atm = {
+  buttons: $("[type='button']"),
+  accounts: {
+    checking: 0,
+    savings: 0
+  },
+  transact: function(event){
+    var account = $(event.target).parent()
+    var userInput = parseInt(account.children("[type='text']").val())
+    var accountName = account.attr("data-account")
+    // check which button we clicked on
+    var button = $(event.target).val()
+    if( button == "Deposit"){
+      // add user input to the relevant account
+      this.accounts[accountName] +=  userInput
+    } else if( button == "Withdraw") {
+      this.accounts[accountName] -=  userInput
+    } else{
+      alert("You don't have enough money")
+    }
+    // update the html for that account
+    account.find(".balance").html("$" + this.accounts[accountName])
+  },
+  listen: function(){
+    this.buttons.on("click", this.transact.bind(this))
+  }
 }
 
-function checkingWithdrawal(amount){
-
-}
-
-function savingsDeposit(amount){
-
-}
-
-function savingsWithdrawal(amount){
-
-}
-
-// an eventListerner for each button, each one a "click"
+atm.listen()
