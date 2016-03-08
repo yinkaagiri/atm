5e17c3ccf30a4914e79a52d76ba2aed013f6cf71
Fatai Agiri <yinka.fatai@gmail.com>
2016-03-08 18:56:39 -0500

    savings dep & withdraw working. Append $ to balance

--- atm.js
+++ atm.js
@@ -1,51 +1,51 @@
 $(document).ready(function(){
 // var checking = new Account($("#cheking"));
 // var checking = new Account($("#savings"));
-var checkingSection = $(".checkingBalance");
+var checkingDisplay = $(".checkingBalance");
 var chekingBalance =0
 var checkingDepositButton = $("#checkingDepositButton");
 var checkingDepositInput = $("#checking_deposit");
 var checkingWithdrawButton = $("#checkingWithdrawButton");
 
-var savingsSection =  $(".savingsBalance");
+var savingsDisplay =  $(".savingsBalance");
 var savingsBalance = 0
 var savingsDepositInput = $("#savingsDepositInput");
 var savingsDepositButton = $("#savingsDepositButton");
-var savingsWithdrawButton = $("savingsWithdrawButton");
+var savingsWithdrawButton = $("#savingsWithdrawButton");
 
 
 function Checking(){
 checkingDepositButton.on("click", function(){
   var checkingInput = checkingDepositInput.val()
   chekingBalance += parseInt(checkingInput);
-  checkingSection.html(chekingBalance);
+  checkingDisplay.html(chekingBalance);
 });
 
 checkingWithdrawButton.on("click", function(){
 var withdraw = chekingBalance - (checkingDepositInput.val());
-checkingSection.html(withdraw);
-console.log(checkingSection.html());
-chekingBalance = parseInt(checkingSection.html());
+checkingDisplay.html(withdraw);
+chekingBalance = parseInt(checkingDisplay.html());
 })
 };
 
-
-
-
-// checkingBalance = parseInt(checking_balance.innerHTML.replace("$", "") );
-
-// get savings deposit value
-savings_amount = document.querySelectorAll("#savings .deposit").value;
-
-// get checking deposit value
-checking_amount = document.querySelectorAll("#savings .deposit").value;
-
-var checkingDeposit = function(checking_amount){
-  checkingBalance = parseInt( checking_balance.innerHTML.replace("$", "") );
-  checkingBalance+= checking_amount;
-  checking_balance.innerHTML = '$'+ checkingBalance;
-  checking_amount = "";
+function Savings(){
+  savingsDepositButton.on("click", function(){
+    var savingsInput = savingsDepositInput.val();
+    savingsBalance += parseInt(savingsInput);
+    savingsDisplay.html(savingsBalance);
+    console.log(savingsBalance);
+  });
+  savingsWithdrawButton.on("click", function(){
+    var savingsWithdraw = savingsBalance - (savingsDepositInput.val());
+    savingsDisplay.html(savingsWithdraw);
+    savingsBalance = parseInt(savingsDisplay.html());
+  });
 }
+
+
+
+
+
 
 function checkingWithdrawal(amount){
 
@@ -60,5 +60,6 @@ function savingsWithdrawal(amount){
 }
 
 Checking();
+Savings();
 // an eventListerner for each button, each one a "click"
 });
